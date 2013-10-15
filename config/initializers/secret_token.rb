157a1d4e94cbca6cebecb4cc011f168aaaca6ee5
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-15 17:36:41 +0300

    + project is added to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZhyviCom::Application.config.secret_token = '2bc5e957abbfdde66777e6ea21b593c071a6c73fb140b41cc1cc203be73d18ae3d9e600dcfa421058cc23b6ad1895503f3294e2be8b3bb9906e64f9c8f7292e9'
