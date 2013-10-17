51f19a37b6ba741bbf095c513ce1d450cbe16163
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-17 07:16:18 +0300

    + some staff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def category_divs
+    Category.all.map { |category|
+      content_tag(:div, category[:name].mb_chars.upcase, class: 'category shadow', 'data-index' => category[:id])
+    }.join.html_safe
+  end
+
+  def category_images
+    Category.all.map { |category|
+      content_tag(:div, category[:url].mb_chars.upcase, class: 'category shadow', 'data-index' => category[:id])
+    }.join.html_safe
+  end
 end
