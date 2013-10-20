7f6a6bed26e8772189e76d1d2795fb9ef216fa02
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-21 02:45:42 +0300

    + validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validators' do
+    it 'failed with empty username and email' do
+      user = FactoryGirl.build(:user, username: nil, email: nil)
+      user.should_not be_valid
+      user.errors.full_messages.should =~ ['Укажите логин или email.']
+    end
+
+    it 'failed with invalid username' do
+      user = FactoryGirl.build(:user, username: '<<<')
+      user.should_not be_valid
+      user.errors.full_messages.should =~ ['Username может состоять только из букв латиницы, цифр и символов "- _ . @".']
+    end
+
+    it 'failed with invalid email' do
+      user = FactoryGirl.build(:user, email: 'wrong_email')
+      user.should_not be_valid
+      user.errors.full_messages.should =~ ['Email не соответствует формату.']
+    end
+  end
 end
