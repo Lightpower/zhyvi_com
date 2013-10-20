505c5734f34cd18a1726821e4b7e964155ede5c6
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-21 02:45:55 +0300

    + seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,26 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+[User, FileLink, Event, Feedback].map { |klass| klass.delete_all }
+
 admin = User.create(username: 'admin', password: '123456', password_confirmation: '123456', role: 'Admin')
 user  = User.create(username: 'test',  password: '123456', password_confirmation: '123456')
+
+puts "Users are added with id: [#{admin.id}, #{user.id}]"
 
 images = ImageLink.create([
     {url: 'http://klads.org.ua/044/1_1.png', user_id: admin.id},
     {url: 'http://klads.org.ua/044/5_4.png', user_id: admin.id}
 ])
 
+puts "#{images.size} images are added"
+
 events = Event.create([
-    {title: 'Мега-событие', preview: 'Это будет МЕГА-событие, не пропустите его!', data: 'Текст события, бла-бла-бла и всё такое', start_at: Time.today - 10.days, user_id: admin.id},
-    {title: 'Просто событие', preview: 'Важное событие, заведённое для теста', data: 'Текст этого события, все дела',   start_at: Time.today + 7.days, user_id: admin.id}
+    {title: 'Мега-событие', preview: 'Это будет МЕГА-событие, не пропустите его!', data: 'Текст события, бла-бла-бла и всё такое', start_at: Time.now - 10.days, user_id: admin.id},
+    {title: 'Просто событие', preview: 'Важное событие, заведённое для теста', data: 'Текст этого события, все дела',   start_at: Time.now + 7.days, user_id: admin.id}
 ])
+
+puts "#{events.size} events are added"
 
 feedbacks = Feedback.create([
   {event_id: events.first.id, user_id: user.id, file_id: images.first.id, data: 'Всё было супер'},
@@ -27,3 +35,4 @@ feedbacks = Feedback.create([
   {event_id: events.first.id, user_id: user.id, data: 'Последний отзыв'}
 ])
 
+puts "#{feedbacks.size} feedbacks are added"
