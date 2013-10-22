43f08b1cbc659094a9d0e7743ea169a5e31dddbf
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-22 23:58:30 +0300

    * update feedback migration

--- db/migrate/20131019183630_create_feedbacks.rb
+++ db/migrate/20131019183630_create_feedbacks.rb
@@ -1,10 +1,11 @@
 class CreateFeedbacks < ActiveRecord::Migration
   def change
     create_table :feedbacks do |t|
-      t.references :event,   index: true,   null: false
-      t.references :user,    index: true,   null: false
-      t.references :file,    index: true
-      t.text :data
+      t.references :event,    index: true,   null: false
+      t.references :user,     index: true,   null: false
+      t.references :file,     index: true
+      t.text       :data
+      t.boolean    :selected, default: false
 
       t.timestamps
     end
