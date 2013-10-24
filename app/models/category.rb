4257f2856f81caf8f6a53931911b43a0d2354631
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-24 13:49:02 +0300

    * .png => .jpg

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,12 @@
 class Category
   ALL = [
-      { id: 1, name: 'Мужественность',   url: '/img/menu/p01.png' },
-      { id: 2, name: 'Женственность',    url: '/img/menu/p02.png' },
-      { id: 3, name: 'Исцеление в паре', url: '/img/menu/p03.png' },
-      { id: 4, name: 'Секс и близость',  url: '/img/menu/p04.png' },
-      { id: 5, name: 'Благосостояние',   url: '/img/menu/p05.png' },
-      { id: 6, name: 'Развитие вместе',  url: '/img/menu/p06.png' },
-      { id: 7, name: 'Живые отношения',  url: '/img/menu/p07.png' }
+      { id: 1, name: 'Мужественность',   url: '/img/menu/p01.jpg' },
+      { id: 2, name: 'Женственность',    url: '/img/menu/p02.jpg' },
+      { id: 3, name: 'Исцеление в паре', url: '/img/menu/p03.jpg' },
+      { id: 4, name: 'Секс и близость',  url: '/img/menu/p04.jpg' },
+      { id: 5, name: 'Благосостояние',   url: '/img/menu/p05.jpg' },
+      { id: 6, name: 'Развитие вместе',  url: '/img/menu/p06.jpg' },
+      { id: 7, name: 'Живые отношения',  url: '/img/menu/p07.jpg' }
   ]
   PREFIX = '/img/menu/'
 
