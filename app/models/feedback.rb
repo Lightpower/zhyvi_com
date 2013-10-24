52a9c4e1e9f4f52e5674d07c0506c2e83cd1bfbf
Vitaly Bezkrovny <bva@aejis.eu>
2013-10-24 12:35:01 +0300

    + selected_list

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -2,4 +2,9 @@ class Feedback < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
   belongs_to :file_link
+
+  def self.selected_list(limit=5)
+    data = where(selected: true).order('created_at desc')
+    data.first(limit) if limit
+  end
 end
