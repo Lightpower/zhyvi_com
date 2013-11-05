b8d6867e23b21b02035ee75ea527bb68be50f8e0
Vitaly Bezkrovny <bva@aejis.eu>
2013-11-05 14:46:48 +0200

    * sing_out bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ZhyviCom::Application.routes.draw do
 
+  devise_for :users
+  root 'home#index'
+
   resources :users
 
   resources :articles
@@ -10,16 +13,11 @@ ZhyviCom::Application.routes.draw do
     end
   end
 
-
   resources :file_links
 
   resources :feedbacks
 
   resources :admins, only: [:index]
-
-  devise_for :users
-  root 'home#index'
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
