06b4398c0b19d02132594416b0ad4bad825085ed
Vitaly Bezkrovny <bva@aejis.eu>
2013-11-13 05:01:05 +0200

    + devise controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
+
+  layout :layout
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -24,5 +27,11 @@ class ApplicationController < ActionController::Base
   #
   def not_found
     raise ActionController::RoutingError.new('Not Found')
+  end
+
+  private
+
+  def layout
+    devise_controller? ? 'devise' : 'application'
   end
 end
